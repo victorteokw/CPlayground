7b4e50895cc53ba51f8d7e031c08f67eb437b977
Victor Teo <victor.teo@fillmula.com>
2021-12-12 16:23:48 +0800

    Initial Commit

---
+++ CPlayground/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CPlayground
+//
+//  Created by Victor Teo on 2021/12/12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
